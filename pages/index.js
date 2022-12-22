fe45162ba2c66c54446dc1ee82a2cb346833d344
Kurt Guardia <kaguardiagamarra@wideagency.com>
2022-12-22 18:29:01 +0100

    components

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,16 @@ export default function Recipes({ recipes }) {
   return (
     <div className='recipe-list'>
       {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} />
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
   )
 }
